45835a00bb4cb38b04f3b9bddc2a8a6fcdc17436
itziarpb <itziar.perezb@gmail.com>
2022-12-05 19:08:19 +0000

    inicio 2

--- src/models.py
+++ src/models.py
@@ -8,12 +8,14 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
+
 
 class Character(Base):
     __tablename__ = 'character'
@@ -22,6 +24,7 @@ class Character(Base):
     gender = Column(String(250), nullable=False)
     hair_color = Column(String(250), nullable=False)
 
+
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
@@ -29,16 +32,27 @@ class Planet(Base):
     gender = Column(String(250), nullable=False)
     hair_color = Column(String(250), nullable=False)
 
-class Favorite(Base):
-    __tablename__ = 'favorite'
+
+class FavoriteCharacter(Base):
+    __tablename__ = 'favoritecharacter'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     character_id = Column(Integer, ForeignKey('character.id'))
     user = relationship(User)
     character = relationship(Character)
 
+
+class FavoritePlanet(Base):
+    __tablename__ = 'favoriteplanet'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    user = relationship(User)
+    planet = relationship(Planet)
+
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
+# Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
